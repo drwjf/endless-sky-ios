5c643289f7bd43a614ae858946558a139c229dfd
Jianfeng Wu <dr.jianfeng.wu@gmail.com>
2024-10-07 10:03:01 -0700

    iOS: add xcode project

---
+++ xcode/EndlessskyiOS/EndlessskyiOS/EndlessskyiOS.h
@@ -0,0 +1,18 @@
+//
+//  EndlessskyiOS.h
+//  EndlessskyiOS
+//
+//  Created by Jianfeng Wu on 10/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EndlessskyiOS.
+FOUNDATION_EXPORT double EndlessskyiOSVersionNumber;
+
+//! Project version string for EndlessskyiOS.
+FOUNDATION_EXPORT const unsigned char EndlessskyiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EndlessskyiOS/PublicHeader.h>
+
+
